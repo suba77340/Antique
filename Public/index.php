0639d22d3b3b7656e7535b15e5afbf4d7951156c
suba <suba01@hotmail.com>
2025-01-23 14:56:11 +0100

    Ajout de Public/index.php

---
+++ Public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Autoloader;
+use App\Config\Main;
+use Dotenv\Dotenv;
+
+// Define the ROOT constant to indicate the root directory of the project
+define('ROOT', dirname(__DIR__));
+
+// Include the autoloader to automatically manage the loading of classes
+require_once __DIR__ . '/../Autoloader.php';
+
+Autoloader::register();
+
+// Load environment variables from the .env file in the root directory
+$dotenv = Dotenv::createImmutable(ROOT);
+$dotenv->load();
+
+// Start the application
+$app = new Main();
+$app->start();
